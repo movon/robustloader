17ed2bc2c97536d0482bc13d3034b86141555438
talbor49 <talbor49@gmail.com>
2019-03-29 22:54:49 +0300

    Fix ownership problem by moving all ownership to main function

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -2,15 +2,21 @@ extern crate xmas_elf;
 
 use std::fs::File;
 use std::io::Read;
+use xmas_elf::ElfFile;
 
-fn read_elf<'a>(path: &str) -> &'a xmas_elf::ElfFile<'a> {
-    let mut elf_bytes:  Vec<u8>  = Vec::new();
-    File::open(path).and_then(|mut f| f.read_to_end(&mut elf_bytes)).expect("Failed to read file");
-    let elf_file = xmas_elf::ElfFile::new(&elf_bytes).expect("Failed to parse ELF");
-    &elf_file
+fn read_file_bytes(path: &str) -> Vec<u8> {
+    let mut elf_bytes   = Vec::new();
+    File::open(path).and_then(|mut f| f.read_to_end( &mut elf_bytes)).expect("Failed to read file");
+    elf_bytes
+}
+
+fn read_elf(bytes: &Vec<u8>) -> ElfFile {
+    let elf_file: ElfFile = ElfFile::new(bytes).expect("Failed to parse ELF");
+    elf_file
 }
 
 fn main() {
-	let elf = read_elf("C:\\dev\\robustloader\\target\\x86_64-bootloader\\release\\robustloader");
+    let elf_bytes = read_file_bytes("C:\\dev\\robustloader\\target\\x86_64-bootloader\\release\\robustloader");
+	let elf = read_elf(&elf_bytes);
     println!("Elf: {:#?}", elf);
 }
