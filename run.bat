2b7ef0f6ba1f7b9c250199d40b162db75577b382
talbor49 <talbor49@gmail.com>
2019-03-30 00:54:17 +0300

    Exit batch script on error

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 rem Run this file from the robustloader folder.
 
 rem Compile the bootloader
-cargo xbuild --target x86_64-bootloader.json --release
-cd builder
+cargo xbuild --target x86_64-bootloader.json --release || exit /b
+cd builder || exit /b
 rem Run the builder that takes the .bootloader section and writes it into an image
-cargo run ..\target\x86_64-bootloader\release\robustloader
-cd ..
+cargo run ..\target\x86_64-bootloader\release\robustloader || exit /b
+cd .. || exit /b
 rem Run that image in qemu
 "C:\Program Files\qemu\qemu-system-x86_64.exe" builder\target\bootloader.img
