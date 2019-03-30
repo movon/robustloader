d42eda5e393a8df771bbc193aa0ee70718684674
RoyChuChun <roy.shulman@gmail.com>
2019-03-30 14:38:06 +0300

    Changed read from disk to use exended read sectors from drive

--- src/asm/utils/16_read_disk.asm
+++ src/asm/utils/16_read_disk.asm
@@ -1,37 +1,11 @@
 .code16
-# load DH num sectors to ES:BX from drive DL
+# Uses DAP to read from the disk
 read_disk_16:
-	mov [SECTORS], dh 	# So we remeber how many we need to read
-	mov ah, 0x02 		# BIOS read sector function
-
-	mov ch, 0x00 		# Select cylinder 0
-	mov dh, 0x00 		# Select head 0(i.e. the first part of the floppy disk)
-	mov cl, 0x02 		# Start reading from the second sector(After the boot sector - each sector is 512 bytes) - 
-			  			# Note second since this is base 1
-
-next_group:
-	mov di, 5 			# When reading from disk, failing is common and we are encouraged to try again
-again:
-	mov ah, 0x02 		# BIOS read sector function
-	mov al, [SECTORS]
-	int 0x13 			# Do the actual read
-	jc maybe_retry 		# If an error occurs the bios will set the carry flag
-
-	sub [SECTORS], al 	# Remaining to read because the bios read the number of read sectors into al
-	jz done
-	mov cl, 0x01 		# Start reading from sector 1 from now on
-	xor dh, 0x01 		# Select the second head, and exit after it
-	jnz next_group
-	inc ch 				# Next cylinder
-	jmp next_group
-maybe_retry:
-	mov ah, 0x00 		# Reset diskdrive
+	mov ah, 0x42
+	mov dl, [BOOT_DRIVE]
+	lea si, DAP
 	int 0x13
-	dec di
-	jnz again
-	mov ah, 0x02 		# Call it again to get the correct return code
-	int 0x13
-	jmp disk_error
+	jc disk_error
 done:
 	lea si, DISK_SUCCESS_MESSAGE
 	call print_string_16
