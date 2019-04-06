a4b06bf475fba27588c67c85ab8e6d109df1db35
Daniel Eliad <daniel.eliad32@gmail.com>
2019-04-06 23:12:12 +0300

    Fixed ident map page table and going to long mode, STARTED USING RUST YAY!

--- src/asm/stages/stage_3.asm
+++ src/asm/stages/stage_3.asm
@@ -6,29 +6,32 @@
 # This stage checks for 64 bit, Sets up paging and enters long mode
 
 stage_3:
+	mov ax, 0x10
+	mov ss, ax
+    mov ds, ax
+    mov es, ax
+
 	call check_cpuid_support
 	call check_long_mode_support
+
+
+
+	cli # disable interrupts
+
+	lidt [zero_idt]
+
+
 	call setup_pae_paging
 	call enable_paging
 
 	lea si, paging_enabled_str
 	call print_string_32
 
-	lgdt [gdt64_info]
-
-	mov ax, 0x10
-	mov ss, ax
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-
-
+	lgdt gdt_64_pointer
 	push 8
 	lea eax, [long_mode_start]
 	push eax
 	retf
-
 	h:
 		jmp h	
 
@@ -88,18 +91,28 @@ setup_pae_paging:
 	# Then it sets the first entry in _pdpt to point to the address of _pdt
 	# with rw + present
 	# At last, it sets every entry(512 entries) in _pdt to rw + present + huge which
-	# means every entry is a page of 2mb which identity allocates the first gigabyte 
-	push ecx
-	mov ecx, 0
+	# means every entry is a page of 2mb which identity allocates the first gigabyte
 
-	lea eax, _pdpt		# Load the adress of the page table diretory pointer into eax
+	# first zero out the page table!
+	lea edi, [__page_table_start]
+	lea ecx, [__page_table_end]
+	sub ecx, edi
+	shr ecx, 2 # one stosd zeros 4 bytes
+	xor eax, eax
+	rep stosd # zero out entire page table
+
+
+	push ecx
+
+	lea eax, [_pdpt]		# Load the adress of the page table diretory pointer into eax
 	or eax, 0x3 		# present + read write page attributes
 	mov [_pmlt4], eax	# map the first entry in the page map level 4 table to the adress
 						# of the page table directory pointer with rw and present attributes
-	lea eax, _pdt		
+	lea eax, [_pdt]
 	or eax, 0x3
 	mov [_pdpt], eax
 
+	mov ecx, 0
 map_pdt_table:
 	mov eax, 0x200000	# 2mb
 	mul ecx 			# multiply eax by the current offset
@@ -119,10 +132,12 @@ enable_paging:
 	# setting the pae flag in cr4
 	# setting the long mode bit in EFER
 	# and setting the paging bit in cr0
+    wbinvd # write back cache
+    mfence # add memory fence
 
 	push ecx
 
-	lea eax, _pmlt4
+	lea eax, [_pmlt4]
 	mov cr3, eax
 
 	mov eax, cr4
@@ -141,17 +156,18 @@ enable_paging:
 	pop ecx
 	ret
 
-gdt64_info:
-	.word gdt64_end - gdt64 - 1
-	.quad gdt64
+.align 4
+zero_idt:
+    .word 0
+    .byte 0
 
-gdt64:
+gdt_64:
 	.word 0                    		# Limit (low).
     .word 0                         # Base (low).
     .byte 0                         # Base (middle)
     .byte 0                         # Access.
     .byte 0                         # Granularity.
-    .byte 0  		
+    .byte 0
 codeseg64:
 	.word 0                         # Limit (low).
     .word 0                         # Base (low).
@@ -166,8 +182,13 @@ dataseg64:
     .byte 0b10010000                # Access (read/write).
     .byte 0b00000000                # Granularity.
     .byte 0                         # Base (high).
-gdt64_end:
 
+.align 4
+    .word 0                              # Padding to make the "address of the GDT" field aligned on a 4-byte boundary
+
+gdt_64_pointer:
+    .word gdt_64_pointer - gdt_64 - 1    # 16-bit Size (Limit) of GDT.
+    .long gdt_64                            # 32-bit Base Address of GDT. (CPU will zero extend to 64-bit)
 
 .include "src/asm/utils/32_print_string.asm"
 
@@ -184,4 +205,6 @@ paging_enabled_str:				.asciz  "Paging is officaly enabled!"
 long_mode_start:
 	mov rax, 0x2f592f412f4b2f4f
     mov [0xb8000], rax
+    jmp stage_4
     hlt
+
---
+++ src/printer/mod.rs
@@ -0,0 +1,3 @@
+pub use self::vga_320x200::*;
+
+mod vga_320x200;
