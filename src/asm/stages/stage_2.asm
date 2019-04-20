36e9d5dbff4ff2df1dcd846bdf27cdb491eb8c2a
RoyChuChun <roy.shulman@gmail.com>
2019-04-20 22:13:15 +0300

    Continued parsing memory map

--- src/asm/stages/stage_2.asm
+++ src/asm/stages/stage_2.asm
@@ -5,7 +5,6 @@
 # This stage sets the target operating mode, loads the kernel from disk,
 # creates an e820 memory map, enters protected mode, and jumps to the
 # third stage.
-
 stage_2:
     lea si, second_stage_start_str
     call print_string_16
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@
 
 use core::panic::PanicInfo;
 use core::fmt::Write;
+use core::mem::size_of;
+use core::slice;
 
 global_asm!(include_str!("asm/stages/stage_1.asm"));
 global_asm!(include_str!("asm/stages/stage_2.asm"));
@@ -29,8 +31,8 @@ fn panic(_info: &PanicInfo) -> ! {
 
 
 extern "C" {
-    static mmap_ent: usize;
-    static _memory_map: usize;
+	static _e820_memory_map_num_entries: u32;
+	static _e820_memory_map_entries: usize;
     static _kib_kernel_size: usize;
     static __page_table_start: usize;
     static __page_table_end: usize;
@@ -53,8 +55,38 @@ pub unsafe extern "C" fn stage_4() -> ! {
     asm!("mov bx, 0x00
           mov ss, bx" ::: "bx" : "intel");
     printer::Printer.clear_screen();
-    write!(printer::Printer, "RUNNING FROM RUST YEAH  :D :))))))) !!!!\t@#!@#!@#!@#!@#!@#\n").unwrap();
-	write!(printer::Printer, "kernel size: {}", _kib_kernel_size).unwrap();
+    write!(printer::Printer, "RUNNING FROM RUST YEAH  :D :))))))) !!!!\t@#!@#!@#!@#!@#!@#\n\n\n\n").unwrap();
+	write!(printer::Printer, "kernel size: 0x{:x}\n", _kib_kernel_size).unwrap();
+	write!(printer::Printer, "0x{:x}\n", _e820_memory_map_num_entries).unwrap();
+
+    parse_mmap();
 
     loop {}
+}
+
+struct MemoryBlock {
+	base_address: u64,
+	region_length: u64,
+    region_type: u32,
+	extended_attribute: u32,
+}
+
+
+enum MemoryBlockType {
+	Usable(MemoryBlock),
+	Reserved(MemoryBlock),
+    ACPIreclaimable(MemoryBlock),
+    APCInvs(MemoryBlock),
+    BadMemory(MemoryBlock),
+}
+
+fn parse_mmap() {
+	let memory_map: *const MemoryBlock = unsafe { _e820_memory_map_entries as *const _ };
+    let memory_array = unsafe { slice::from_raw_parts(memory_map, (_e820_memory_map_num_entries as usize) * size_of::<MemoryBlockType>()) };
+    let num_entries = unsafe { _e820_memory_map_num_entries };
+    let mut index = 0;
+    for i in 0 .. num_entries {
+        let base_address: u64 = unsafe { (*memory_map).extended_attribute as u64 };
+        write!(printer::Printer, "0x{:x}", base_address).unwrap();
+    }
 }
